1fdb996a63b6753402658d8d6d44068b7662aa80
Yan <201421070138@outlook.com>
2018-11-06 18:27:20 +0800

    201811061827

--- YLogging.h
+++ YLogging.h
@@ -4,4 +4,54 @@
 #include "LogStream.h"
 #include "Timestamp.h"
 
+namespace YBASE
+{
+    class TimeZone;
+
+    class Logger
+    {
+    public:
+        enum LogLevel
+        {
+            TRACE,
+            DEBUG,
+            INFO,
+            WARN,
+            ERROR,
+            FATAL,
+            NUM_LOG_LEVELS,
+        };
+        class SourceFile
+        {
+        public:
+            //将N当作常数来传进使用
+            template<int N>
+            SourceFile(const char (&arr)[N]):data_(arr), size_(N-1)
+            {
+                const char* slash = strrchr(data_, '/');
+                if(slash)
+                {
+                    data_ = slash + 1;
+                    size_ -= static_cast<int>(data_ - arr);
+                }
+            }
+
+            explicit SourceFile(const char* filename):data_(filename)
+            {
+                const char* slash = strrchr(filename, '/');
+                if(slash)
+                {
+                    data_ = slash + 1;
+                }
+                size_ = static_cast<int>(strlen(data_));
+            }
+
+            const char* data_;
+            int size_;
+        };
+
+        
+    }
+}
+
 #endif
