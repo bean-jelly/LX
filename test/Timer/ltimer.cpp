c368b9163f42e4f2185e45aacb7fdbea35e9c606
bean-jelly <201421070138@outlook.com>
2018-12-23 14:36:10 +0800

    201812231436

--- test/Timer/ltimer.cpp
+++ test/Timer/ltimer.cpp
@@ -75,7 +75,7 @@ void TimerManager::RemoveTimer(Timer* timer)
 void TimerManager::DetectTimers()
 {
     unsigned long long now = GetCurrentMillisecs();
-    while(!heap_.empty() && m_heap[0].time <= now)
+    while(!m_heap.empty() && m_heap[0].time <= now)
     {
         Timer* timer = m_heap[0].timer;
         RemoveTimer(timer);
