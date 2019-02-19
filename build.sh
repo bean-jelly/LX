345372f9969951e3f78172f4e8d2cac1008bcd00
Yan <201421070138@outlook.com>
2019-02-19 13:40:00 +0800

    201902191339

--- build.sh
+++ build.sh
@@ -8,6 +8,8 @@ BUILD_TYPE=${BUILD_TYPE:-release}
 INSTALL_DIR=${INSTALL_DIR:-../${BUILD_TYPE}-install-cpp11}
 BUILD_NO_EXAMPLES=${BUILD_NO_EXAMPLES:-0}
 
+ln -sf $BUILD_DIR/$BUILD_TYPE-cpp11/compile_commands.json
+
 mkdir -p $BUILD_DIR/$BUILD_TYPE-cpp11 \
     && cd $BUILD_DIR/$BUILD_TYPE-cpp11 \
     && cmake \
