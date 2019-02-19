3e512c4e5afbb78e55afa4d4cda240d4cf203d54
Yan <201421070138@outlook.com>
2019-02-19 11:45:53 +0800

    201902191145

--- build.sh
+++ build.sh
@@ -2,8 +2,17 @@
 
 set -x
 
-SOURCE_DIR = pwd
+SOURCE_DIR = `pwd`
 BUILD_DIR=${BUILD_DIR:-../build}
 BUILD_TYPE=${BUILD_TYPE:-release}
 INSTALL_DIR=${INSTALL_DIR:-../${BUILD_TYPE}-install-cpp11}
 BUILD_NO_EXAMPLES=${BUILD_NO_EXAMPLES:-0}
+
+mkdir -p $BUILD_DIR/$BUILD_TYPE-cpp11 \
+    && cd $BUILD_DIR/$BUILD_TYPE-cpp11 \
+    && cmake \
+        -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+        -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
+        -DCMAKE_BUILD_NO_EXAMPLES=$BUILD_NO_EXAMPLES \
+        $SOURCE_DIR \
+    && make $*
