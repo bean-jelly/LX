0dca77fb00674d7bd686381b2e1a2e18dff17c7a
Yan <201421070138@outlook.com>
2019-02-26 17:05:09 +0800

    add acceptor

---
+++ net/YAcceptor.h
@@ -0,0 +1,43 @@
+#ifndef LX_NET_ACCEPTOR_H
+#define LX_NET_ACCEPTOR_H
+
+#include <functional>
+
+#include <LX/net/YChannel.h>
+#include <LX/net/YSocket.h>
+
+namespace LX
+{
+    namespace net
+    {
+        class EventLoop;
+        class InetAddress;
+
+        class Acceptor : noncopyable
+        {
+        public:
+            typedef std::function<void (int sockfd, const InetAddress&)> NewConnectionCallback;
+
+            Acceptor(EventLoop* loop, const InetAddress& listenAddr, bool reuseport);
+            ~Acceptor();
+
+            void setNewConnectionCallback(const NewConnectionCallback& cb)
+            { newConnectionCallback_ = cb; }
+
+            bool listenning() const { return listenning_; }
+            void listen();
+
+        private:
+            void handleRead();
+
+            EventLoop* loop_;
+            Socket acceptSocket_;
+            Channel acceptChannel_;
+            NewConnectionCallback newConnectionCallback_;
+            bool listenning_;
+            int idleFd_;
+        };
+    }
+}
+
+#endif
